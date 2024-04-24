0b858cc1344c58a41dfb0f361e8e188afbf8d28c
Picorims <picorims.contact@gmail.com>
2024-04-24 19:08:50 +0200

    add synchronization and display debugging

--- src/read_pixels_parall.cpp
+++ src/read_pixels_parall.cpp
@@ -5,14 +5,21 @@
 #include <chrono>
 #include <thread>
 #include <unistd.h>
+
+#ifdef WIRING_PI
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
+#endif
 
 #define WIDTH 100
 #define HEIGHT 100
+#define FPS 10
 #define BUFFER_SIZE 10
-bool verbose = false;
-bool spiEnabled = true;
+bool flagVerbose = false;
+bool flagSpiEnabled = true;
+bool flagGraphicDisp = false;
+bool flagDumpBuffer = false;
+bool flagVerboseBuffer = false;
 
 // Initialisation de WiringPi SPI
 const int SPI_CHANNEL = 0; // Utilisez le canal 0 de SPI
@@ -28,13 +35,44 @@ private:
     short buffer[BUFFER_SIZE][WIDTH][HEIGHT] = {};
     bool readable_frames[BUFFER_SIZE] = {};
     int read_pos = 0;
-    int write_pos = BUFFER_SIZE - 1; //next write pos is 0
+    int write_pos = 0;
 
 public:
     circular_buffer_t() {
         for (int i = 0; i < BUFFER_SIZE; i++) {
             readable_frames[i] = false;
         }
+    }
+
+    void print_readable_frames() {
+        std::cout << "Readable frames: ";
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            std::cout << readable_frames[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void dump_buffer() {
+        cv::Mat dumpImg(HEIGHT, WIDTH*BUFFER_SIZE, CV_8UC3, cv::Scalar(0, 0, 0));
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                for (int k = 0; k < HEIGHT; k++) {
+                    short px = buffer[i][j][k];
+                    int r = ((px & 0b0000111100000000) >> 8) * 16;
+                    int g = ((px & 0b0000000011110000) >> 4) * 16;
+                    int b = ((px & 0b0000000000001111)) * 16;
+                    dumpImg.at<cv::Vec3b>(k, i*WIDTH+j) = cv::Vec3b(b,g,r);
+                }
+            }
+        }
+        cv::imshow("Dump", dumpImg);           
+    }
+
+    bool empty() {
+        for (int i = 0; i < BUFFER_SIZE; i++) {
+            if (readable_frames[i]) return false;
+        }
+        return true;
     }
 
     int next_read_pos() {
@@ -48,11 +86,19 @@ public:
     void incr_read_pos() {
         readable_frames[read_pos] = false;
         read_pos = next_read_pos();
+        if (flagVerboseBuffer) {
+            std::cout << "Read pos: " << read_pos << std::endl;
+            print_readable_frames();
+        }
     }
 
     void incr_write_pos() {
         readable_frames[write_pos] = true;
         write_pos = next_write_pos();
+        if (flagVerboseBuffer) {
+            std::cout << "Write pos: " << write_pos << std::endl;
+            print_readable_frames();
+        }
     }
 
     bool next_write_pos_available() {
@@ -66,6 +112,7 @@ public:
     short read(int i, int j) {
         assert(i >= 0 && j >= 0);
         assert(i < WIDTH && j < HEIGHT);
+        assert(readable_frames[read_pos]);
 
         return buffer[read_pos][i][j];
     }
@@ -73,6 +120,7 @@ public:
     void write(int i, int j, short v) {
         assert(i >= 0 && j >= 0);
         assert(i < WIDTH && j < HEIGHT);
+        assert(!readable_frames[write_pos]);
 
         buffer[write_pos][i][j] = v;
     }
@@ -91,14 +139,16 @@ void debug_px(short px) {
 }
 
 void send_px(short px) {
-    if (verbose) debug_px(px);
+    if (flagVerbose) debug_px(px);
 
-    if (spiEnabled) {
+    if (flagSpiEnabled) {
         // Préparation des données pour l'envoi via SPI
         unsigned char data[2];
         data[0] = px >> 8; // MSB
         data[1] = px & 0xFF; // LSB
+        #ifdef WIRING_PI
         wiringPiSPIDataRW(SPI_CHANNEL, data, 2);
+        #endif
     }
 }
 
@@ -111,25 +161,47 @@ void process_frame_section(cv::Mat frame, int startRow, int endRow, int nbFrames
             int g16 = color[1] / 16;
             int b16 = color[0] / 16;
             short pixel = (r16 << 8) + (g16 << 4) + b16;
-            buf->write(i,j,pixel);
+            buf->write(j,i,pixel);
         }
     }
 }
 
+long long now_micros() {
+    //see: https://stackoverflow.com/a/2834294
+    //from: https://stackoverflow.com/questions/2831841/how-to-get-the-time-in-milliseconds-in-c?answertab=trending#tab-top
+    namespace sc = std::chrono;
+
+    auto time = sc::high_resolution_clock::now(); // get the current time
+
+    auto since_epoch = time.time_since_epoch(); // get the duration since epoch
+
+    // I don't know what system_clock returns
+    // I think it's uint64_t nanoseconds since epoch
+    // Either way this duration_cast will do the right thing
+    auto micros = sc::duration_cast<sc::microseconds>(since_epoch);
+
+    long long now = micros.count(); // just like java (new Date()).getTime();
+    return now;
+}
+
 int main(int argc, char** argv) {
     if(argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <video_file_path>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <video_file_path> [-v] [--no-spi] [-g] [-b]" << std::endl;
         return -1;
     }
     if (argc > 2) {
         for (int i = 2; i < argc; i++) {
-            if (std::string(argv[i]) == "-v") verbose = true;
-            if (std::string(argv[i]) == "--no-spi") spiEnabled = false;
+            if (std::string(argv[i]) == "-v") flagVerbose = true;
+            if (std::string(argv[i]) == "--no-spi") flagSpiEnabled = false;
+            if (std::string(argv[i]) == "-g") flagGraphicDisp = true;
+            if (std::string(argv[i]) == "-b") flagDumpBuffer = true;
+            if (std::string(argv[i]) == "-vb") flagVerboseBuffer = true;
         }
     }
 
     // Initialisation de WiringPi et SPI
-    if (spiEnabled) {
+    if (flagSpiEnabled) {
+        #ifdef WIRING_PI
         if (wiringPiSetup() == -1) {
             std::cerr << "Failed to initialize wiringPi" << std::endl;
             return -1;
@@ -138,53 +210,123 @@ int main(int argc, char** argv) {
             std::cerr << "Failed to setup SPI" << std::endl;
             return -1;
         }
+        #endif
     }
 
+    // open video with OpenCV
     cv::VideoCapture cap(argv[1]);
     if(!cap.isOpened()) {
         std::cerr << "Error: Could not open video." << std::endl;
         return -1;
     }
 
+    //prepare graphical display
+    cv::Mat canvas(HEIGHT, WIDTH, CV_8UC3, cv::Scalar(0, 0, 0));
+
+    // sync init
+    long long pxIntervalMicros = 1000000 /*micros*/ / (FPS * WIDTH * HEIGHT);
+    int cursorX = 0;
+    int cursorY = 0;
+    auto now = now_micros();
+    auto then = now_micros();
+    auto videoStart = now_micros();
+    auto ellapsed = then - now;
+
+    // other initialization
     auto start = std::chrono::system_clock::now();
     int nbFrames = 0;
+    bool refreshDisp = false;
+    bool cachedAllVideo = false;
     std::shared_ptr<circular_buffer_t> frame_buffer = std::make_shared<circular_buffer_t>();
 
+
     while(true) {
-        if (frame_buffer->next_write_pos_available()) {
+        now = now_micros();
+        auto ellapsedMicros = now - then;
+        ellapsed = now - videoStart;
+
+        if (frame_buffer->next_write_pos_available() && !cachedAllVideo && (ellapsedMicros < pxIntervalMicros || frame_buffer->empty())) {
             frame_buffer->incr_write_pos();
             cv::Mat frame;
             cap >> frame;
-            if(frame.empty()) break;
-            nbFrames++;
+            if (frame.empty()) {
+                std::cout << "Cached all video" << std::endl;
+                cachedAllVideo = true;
+            } else {
+                nbFrames++;
+                if (flagVerboseBuffer) std::cout << "Frame " << nbFrames << " being cached." << std::endl;
 
-            int numThreads = std::thread::hardware_concurrency();
-            std::vector<std::thread> threads;
-            int rowsPerThread = frame.rows / numThreads;
+                int numThreads = std::thread::hardware_concurrency();
+                std::vector<std::thread> threads;
+                int rowsPerThread = frame.rows / numThreads;
 
-            for(int i = 0; i < numThreads; ++i) {
-                int startRow = i * rowsPerThread;
-                int endRow = (i + 1) * rowsPerThread;
-                if(i == numThreads - 1) endRow = frame.rows;
+                for(int i = 0; i < numThreads; ++i) {
+                    int startRow = i * rowsPerThread;
+                    int endRow = (i + 1) * rowsPerThread;
+                    if(i == numThreads - 1) endRow = frame.rows;
 
-                threads.emplace_back(process_frame_section, frame.clone(), startRow, endRow, nbFrames, frame_buffer);
-            }
-
-            for(auto& thread : threads) {
-                thread.join();
-            }
-
-            if(cv::waitKey(30) >= 0) break;
-        } else {
-            if (frame_buffer->current_frame_readable()) {    
-                for (int i = 0; i < WIDTH; i++) {
-                    for (int j = 0; j < HEIGHT; j++) {
-                        send_px(frame_buffer->read(i,j));
-                    }
-                frame_buffer->incr_read_pos();
+                    threads.emplace_back(process_frame_section, frame.clone(), startRow, endRow, nbFrames, frame_buffer);
                 }
+
+                for(auto& thread : threads) {
+                    thread.join();
+                }
+
+                if(cv::waitKey(30) >= 0) break;
+            }
+        } else {
+            if (frame_buffer->current_frame_readable()) {  
+                if (ellapsedMicros > pxIntervalMicros) {
+                    // send
+                    send_px(frame_buffer->read(cursorX,cursorY));
+                    if (flagGraphicDisp) {
+                        auto px = frame_buffer->read(cursorX,cursorY);
+                        int r = ((px & 0b0000111100000000) >> 8) * 16;
+                        int g = ((px & 0b0000000011110000) >> 4) * 16;
+                        int b = ((px & 0b0000000000001111)) * 16;
+
+                        canvas.at<cv::Vec3b>(cursorY, cursorX) = cv::Vec3b(b,g,r);
+                    }
+        
+                    // update state
+                    int pixelsEllapsed = ellapsedMicros / pxIntervalMicros;
+                    if (pixelsEllapsed > 100) std::cout << "skipping " << pixelsEllapsed-1 << " pixels." << std::endl;
+                    while (pixelsEllapsed > 10000) {
+                        pixelsEllapsed -= 10000;
+                        frame_buffer->incr_read_pos();
+                    }
+
+                    auto oldX = cursorX;
+                    cursorX = (cursorX + pixelsEllapsed) % WIDTH;
+                    
+                    //Changed of line?
+                    if (cursorX < oldX) {
+                        auto oldY = cursorY;
+                        cursorY = (cursorY + 1) % 100;
+
+                        //Changed of frame?
+                        if (cursorY < oldY) {
+                            frame_buffer->incr_read_pos();
+                            refreshDisp = true;
+                        }
+                    }
+
+                    then = now;
+                }
+            } else if (cachedAllVideo) {
+                break;
             }
         }
+        if (flagGraphicDisp && refreshDisp) {
+            cv::Mat scaledCanvas;
+            const int SCALE = 4; 
+            cv::resize(canvas, scaledCanvas, cv::Size(SCALE*HEIGHT, SCALE*WIDTH), SCALE, SCALE, cv::INTER_NEAREST);
+            cv::imshow("Display", scaledCanvas);
+            if (flagDumpBuffer) frame_buffer->dump_buffer();
+            cv::waitKey(1);
+            refreshDisp = false;
+        }
+
     }
 
     auto stop = std::chrono::system_clock::now();
@@ -196,6 +338,7 @@ int main(int argc, char** argv) {
     std::cout << "Per frame (ms): " << timeMsPerFrame << std::endl;
     double timeMsPerPixel = (timeMsPerFrame / static_cast<double>(WIDTH * HEIGHT));
     std::cout << "Per pixel (ms): " << timeMsPerPixel << std::endl;
+    if (flagGraphicDisp) cv::waitKey(30000);
 
     cap.release();
     cv::destroyAllWindows();
